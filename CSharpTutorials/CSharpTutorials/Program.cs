8fadb8803da49937e4cd96a10397eafc1cdb6320
Serhat OMAÇ <91917542+omacserhat@users.noreply.github.com>
2021-10-06 17:57:43 +0300

    CSharpTutorialsGun1

---
+++ CSharpTutorials/CSharpTutorials/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!";
+
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
---
+++ CSharpTutorials/Keywords/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keywords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Access Modifier Keywords
+
+            //public:herkesin erişebildiği.
+            //internal class oluşturulduğunda otomatik gelen.
+            //private özel erişim, yalnızca aynı sınıf ve aynı yapıdaki kodlar erişebilir.
+            //protected korumalı değiştirici. aynı sınıftaki veya o sınıftan türetilen bir sınıftaki kodların türe veya üyelerine erişmesine izin verir.
+
+            //Modifier Keywords
+
+            //ABSTRACT :Karmaşıklığı yönetmek için kullanılır. Nesnenin diğer tüm nesne türlerinden ayıran temel özelliklerini belirtir 
+            //ABSTRACT : örn: telefon--> arama , mesaj atma özellikleri 
+
+            //ASYNC : içerisinde asenkron işlem yapılacak methodu belirtir.
+
+            //CONST :Bir değişkenin değerinin program boyunca sabit olarak tutulması istendiğinde const (sabit) ifadesinden yararlanılır.
+            //Tanımlandığı satırda değeri atanmalıdır.
+            //Pi sayısı, ışık hızı gibi belirli değerlere sahip veriler kullanılarak bir program yazılacaksa, const yapısını kullanmak kolaylık sağlar
+
+            //EVENT: event anahtar kelimesi delegate anahtar kelimesi ile tanımlanan bir değişkenin
+            //olay olduğunu açıkca belirtmek ve yönetmek için kullanılan anahtar kelimedir.
+
+            //new:bir türün yeni bir örneğini oluşturmak için kullanılır.
+            //new yeni bir oluşturucu olarakta kullanılır.
+
+            //OVERRİDE: "virtual" metotlarda kullanılırlar. Override yapısı ile miras aldığı metotları ezer.
+            //Mevcut metodun yerine aynı isimli olan, diğer metodu çalıştırır.
+
+            //partial
+            //readonly
+            //sealed
+            //static
+            //unsafe
+            //virtual
+            //volatile
+
+            //Statement Keywords
+
+            //if else
+            //switch case
+            //do
+            //for
+            //foreach
+            //in
+            //while
+            //breack
+            //continue
+            //default
+            //goto
+            //return
+            //yield
+            //throw
+            //try catch
+            //finally
+            //checked unchecked
+            //fixed
+            //lock
+
+
+            //Method Parameter Keywords
+
+            //params
+            //ref
+            //out
+
+            //Namespace Keywords
+
+            //using
+            //.operator
+            //::operator
+            //extern alias
+
+
+            //Operator Keywords
+
+            //as
+            //await
+            //is
+            //new
+            //sizeof
+            //typeof
+            //stackalloc
+            //checked
+            //unchecked
+
+            //Access Keywords
+
+            //base
+            //this
+
+            //Literal Keywords
+
+            //null
+            //false
+            //true
+            //value
+            //void
+
+            //Type Keywords
+
+            //bool
+            //byte
+            //char
+            //class
+            //decimal
+            //double
+            //enum
+            //float
+            //int
+            //long
+            //sbyte
+            //short
+            //string
+            //struct
+            //uint
+            //ulong
+            //ushort
+
+            //Contextual Keywords
+
+            //add
+            //var
+            //dynamic
+            //global
+            //set
+            //value
+
+            //Query Keywords
+
+            //from
+            //where
+            //select
+            //group
+            //into
+            //orderby
+            //join
+            //let
+            //in
+            //on
+            //equals
+            //by
+            //ascending
+            //descending
+            //descending
+
+        }
+    }
+}
