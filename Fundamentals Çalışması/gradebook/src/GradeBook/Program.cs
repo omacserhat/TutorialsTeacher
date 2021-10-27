4e15c42e7f219a1a5db54bf38631253089b56ee0
Serhat OMAÇ <91917542+omacserhat@users.noreply.github.com>
2021-10-27 17:20:17 +0300

    -

---
+++ Fundamentals Çalışması/gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book("Serhat's Grade Book");
+            book.AddGrade(89.1);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            
+            var stats = book.GetStatistics();
+            Console.WriteLine($"En Büyük Sayı: {stats.High}");
+            Console.WriteLine($"En Küçük Sayı: {stats.Low}");
+            Console.WriteLine($"Sayıların ortalaması: {stats.Average:N1}");
+
+        }
+    }
+}
